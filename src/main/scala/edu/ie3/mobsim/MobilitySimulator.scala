3e979849547fd438bf9de43786351e964f982207
Marius Staudt <marius.staudt@tu-dortmund.de>
2022-08-15 10:00:47 +0200

    Resolving conflicts.

--- src/main/scala/edu/ie3/mobsim/MobilitySimulator.scala
+++ src/main/scala/edu/ie3/mobsim/MobilitySimulator.scala
@@ -177,6 +177,8 @@ final class MobilitySimulator(
       builder.addArrival(cs, updatedEv)
     }
 
+    updateElectricVehicles(arrivals)
+
     // compile map from evcs to their parked evs
     val evcsToParkedEvs = electricVehicles
       .flatMap { ev =>
