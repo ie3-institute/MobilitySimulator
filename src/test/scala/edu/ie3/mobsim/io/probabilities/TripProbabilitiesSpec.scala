fbdf04ff319290b61ef220070781e4d5e8e6b7fb
Thomas <thomas.oberliessen@googlemail.com>
2022-11-07 12:10:22 +0100

    copy round 15 to parking time

--- src/test/scala/edu/ie3/mobsim/io/probabilities/TripProbabilitiesSpec.scala
+++ src/test/scala/edu/ie3/mobsim/io/probabilities/TripProbabilitiesSpec.scala
@@ -26,7 +26,7 @@ class TripProbabilitiesSpec extends UnitSpec {
     val pathsAndSources =
       PathsAndSources("testSim", Input(None, gridConf, mobilityConf), None)
     val tripProbabilities =
-      TripProbabilities.read(pathsAndSources, ",", round15 = false)
+      TripProbabilities.read(pathsAndSources, ",", round15 = true)
 
     tripProbabilities.categoricalLocation.probabilitiesWeekday.size shouldBe 42
     tripProbabilities.categoricalLocation.probabilitiesSaturday.size shouldBe 42
