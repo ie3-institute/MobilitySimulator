72619491761058aae8f1c7e29e8d9af27ecb44d2
danielfeismann <daniel.feismann@tu-dortmund.de>
2025-11-11 21:44:30 +0100

    Added basic readTheDocs structure for documentation

---
+++ docs/readthedocs/conf.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+project = u'MobilitySimulator'
+copyright = u'2023. TU Dortmund University, Institute of Energy Systems, Energy Efficiency and Energy Economics, Research group Distribution grid planning and operation '
+version = '1.0'
+release = '1.0.0'
+
+# General options
+needs_sphinx = '1.0'
+master_doc = 'index'
+pygments_style = 'tango'
+add_function_parentheses = True
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.autodoc',
+    'sphinxcontrib.plantuml',
+    'sphinx.ext.intersphinx',
+    'hoverxref.extension',
+    'sphinxcontrib.bibtex'
+]
+
+# Make sure the target is unique
+autosectionlabel_prefix_document = True
+
+myst_enable_extensions = ["dollarmath", "amsmath"]
+
+templates_path = ['_templates']
+exclude_trees = ['.build']
+source_suffix = ['.md']
+source_encoding = 'utf-8-sig'
+
+
+# HTML options
+html_theme = 'sphinx_rtd_theme'
+html_short_title = "mobsim"
+htmlhelp_basename = 'mobsim-doc'
+html_use_index = False
+html_show_sourcelink = False
+html_static_path = ['_static']
+
+# PlantUML options
+plantuml = 'plantuml'
---
+++ docs/readthedocs/index.md
@@ -0,0 +1,11 @@
+# Welcome to MobilitySimulator docs
+
+Welcome to the documentation of MobilitySimulator - a simulator of vehicle movements developed at the
+Institute of Energy Systems, Energy Efficiency and Energy Economics at TU Dortmund University, Germany.
+
+```{toctree}
+---
+maxdepth: 2
+---
+about
+```
